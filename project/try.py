2d7deb56eb47ccb64ab2686f7ba4bdc6ccc635ec
brightbortey <brightbortey76@gmail.com>
2019-07-22 15:07:36 +0100

    GC Project

---
+++ project/try.py
@@ -0,0 +1,34 @@
+from gpiozero import LED, Button, Buzzer
+from time import sleep
+
+buzzer = Buzzer(15)
+button = Button(21)
+light = LED(25)
+
+global people
+global counter
+
+button.when_pressed =light.on
+button.when_released = light.off
+counter = 0
+people = 0
+def countdown(n):
+    while n > 0:
+        i="a"
+        button.wait_for_press()
+        buzzer.on()
+        sleep(1)
+        buzzer.off()
+        print("Pressed")
+        #i=input('enter a letter: ')
+        if i =="a":
+            print(n)
+            n = n + 1
+#            people.save_value({'value':n})
+        
+            
+
+        
+while True:
+    
+    countdown(1)
