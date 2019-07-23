214faa73733a43d0328f49611dae8bd378c72746
brightbortey <brightbortey76@gmail.com>
2019-07-23 17:20:59 +0100

    GC Project

--- project/website/myapp.py
+++ project/website/myapp.py
@@ -1,7 +1,23 @@
 
 from flask import Flask, render_template
+import paho.mqtt.client as mqtt
+
+import concurrent.futures
 #_____________1. init app_______________
 app = Flask(__name__)
+studentnumber = 0
+
+def on_connect(client,userdata,flag,rc): 
+    print("Connected with result code"+str(rc))
+
+    client.subscribe("#")
+
+def on_message(client,userdata,msg):
+    global studentnumber
+    print(msg.topic + " \n " + msg.payload.decode("utf-8") + "\n")
+    studentnumber=msg.payload.decode("utf-8")
+    
+
 
 #___________2.   Routers________________
 
@@ -11,10 +27,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    print("here")
+    global studentnumber
+    print(studentnumber)
+    return render_template('index.html',students = studentnumber )
 
-
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    print("here")
+    global studentnumber
+    print(studentnumber)
+    return jsonify(students = studentnumber)
+    
+def mqttloop(client):
+    client.loop_forever()
 
 #__________3.start server_____________________
 if __name__== '__main__':
-   app.run(debug=True, host='0.0.0.0')
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("127.0.0.1",1883,60)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+    executor.submit(mqttloop, client)
+    print("blaa")
+    app.run(debug=True, host='0.0.0.0', port="1234")
